6e096bed8613909a244f7a330d443399a7559044
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-22 14:40:49 +0000

    Completing Sprint-1/fix/median.test.js

--- Sprint-1/fix/median.test.js
+++ Sprint-1/fix/median.test.js
@@ -4,6 +4,28 @@
 // passing all the tests...
 // Fix the implementation of calculateMedian so it passes all tests
 
+/*const calculateMedian = require("./median.js");
+
+describe("calculateMedian", () => {
+  test("returns the median for odd length array", () => {
+    expect(calculateMedian([1, 2, 3])).toEqual(2);
+    expect(calculateMedian([1, 2, 3, 4, 5])).toEqual(3);
+  });
+
+  test("returns the average of middle values for even length array", () => {
+    expect(calculateMedian([1, 2, 3, 4])).toEqual(2.5);
+    expect(calculateMedian([1, 2, 3, 4, 5, 6])).toEqual(3.5);
+  });
+
+  test("doesn't modify the input", () => {
+    const list = [1, 2, 3];
+    calculateMedian(list);
+
+    expect(list).toEqual([1, 2, 3]);
+  });
+});*/
+
+//Answer:
 const calculateMedian = require("./median.js");
 
 describe("calculateMedian", () => {
@@ -24,3 +46,4 @@ describe("calculateMedian", () => {
     expect(list).toEqual([1, 2, 3]);
   });
 });
+
