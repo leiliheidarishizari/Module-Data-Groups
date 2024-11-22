fb4b28478aa8527cc103847c79db326846523eba
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-22 16:26:24 +0000

    Amending Sprint-1/implement/dedupe.js

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -2,5 +2,7 @@ function dedupe(arr) {
     return [...new Set(arr)];
   }
   
-  console.log(dedupe([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
+  console.log(dedupe([1, 2, 2, 3, 4, 4, 5])); 
+  
+  module.exports = dedupe;// [1, 2, 3, 4, 5]
   
