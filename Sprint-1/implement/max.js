aab891e49d948e662c9bc70f927198a52e3961f1
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-22 21:01:22 +0000

    Completing Sprint-1/implement/max.js

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,16 @@
 function findMax(elements) {
-}
-
-module.exports = findMax;
+    // Filter the array to only include numeric values
+    const numericElements = elements.filter((item) => typeof item === "number");
+  
+    // Return -Infinity if the array is empty
+    if (numericElements.length === 0) {
+      return -Infinity;
+    }
+  
+    // Find and return the maximum number in the numeric elements
+    return Math.max(...numericElements);
+  }
+  
+  module.exports = findMax;
+  
+      
