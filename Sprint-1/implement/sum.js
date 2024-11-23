0ae83d837b0657340dcd1c7e0f99fdc6f34ab159
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-23 15:42:36 +0000

    Completing Sprint-1/implement/sum.js

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,9 @@
 function sum(elements) {
-}
-
-module.exports = sum;
+    // Filter for numerical elements and sum them
+    return elements
+      .filter((element) => typeof element === "number")
+      .reduce((acc, num) => acc + num, 0); // Start with 0 as the initial sum
+  }
+  
+  module.exports = sum;
+  
