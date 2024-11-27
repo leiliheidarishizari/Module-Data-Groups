2784338f255191166b1113935babe2924754b20d
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-27 18:28:06 +0000

    Completing Sprint-2/debug/author.js

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -3,6 +3,11 @@
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
+//Answer :
+//The for...of loop is designed to iterate over iterables, such as arrays, strings, and maps.
+// An object like author is not iterable by default, so using for...of directly on an object will result in an error.
+//So,we should use method like Object.values().
+
 const author = {
   firstName: "Zadie",
   lastName: "Smith",
@@ -11,6 +16,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
