6d36f61ccaf09b6b20fcf89b8fe3551941a4cf87
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-28 18:46:49 +0000

    Completing Sprint-2/implement/querystring.js

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -3,14 +3,23 @@ function parseQueryString(queryString) {
   if (queryString.length === 0) {
     return queryParams;
   }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    const separatorIndex = pair.indexOf("=");
+    if (separatorIndex === -1) {
+      // No "=" in the pair, treat it as a key with an empty value
+      queryParams[pair] = "";
+    } else {
+      const key = pair.slice(0, separatorIndex);
+      const value = pair.slice(separatorIndex + 1);
+      queryParams[key] = value;
+    }
   }
 
   return queryParams;
 }
 
 module.exports = parseQueryString;
+
