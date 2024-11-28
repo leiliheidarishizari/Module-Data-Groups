a306371f80ae7a8a35054ea8a388fe7c52b7a5f9
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-11-28 18:48:56 +0000

    Completing Sprint-2/implement/querystring.test.js

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -3,10 +3,42 @@
 // Below is one test case for an edge case the implementation doesn't handle well.
 // Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
 
-const parseQueryString = require("./querystring.js")
+const parseQueryString = require("./querystring.js");
 
 test("parses querystring values containing =", () => {
   expect(parseQueryString("equation=x=y+1")).toEqual({
     "equation": "x=y+1",
+  });
+});
+
+test("parses keys without values", () => {
+  expect(parseQueryString("key1&key2=")).toEqual({
+    "key1": "",
+    "key2": "",
+  });
+});
+
+test("parses empty query strings", () => {
+  expect(parseQueryString("")).toEqual({});
+});
+
+test("parses multiple key-value pairs", () => {
+  expect(parseQueryString("key1=value1&key2=value2")).toEqual({
+    "key1": "value1",
+    "key2": "value2",
+  });
+});
+
+test("parses keys or values with special characters", () => {
+  expect(parseQueryString("key%201=value%202&key=value")).toEqual({
+    "key%201": "value%202",
+    "key": "value",
+  });
+});
+
+test("handles no '=' in a key-value pair", () => {
+  expect(parseQueryString("key1&key2=value2")).toEqual({
+    "key1": "",
+    "key2": "value2",
   });
 });
